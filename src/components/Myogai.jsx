97c5bf7cf46da47af8fdd619ca4de7a49c517d43
nicrandy <nicholasresch@gmail.com>
2022-09-30 16:42:36 -0500

    updated myogai

---
+++ src/components/Myogai.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+export const Myogai = () => {
+  return (
+    <div className="myogai">
+      <div className="nameAndImg">
+        <p>Myogai</p>
+        <img
+          src="https://i.pinimg.com/originals/64/41/61/644161b5ce15397473f2c4a49620ee8f.gif"
+          alt="person doing a yoga pose"
+          onClick={()=> window.open("www.myogai.com", "_blank")}
+        ></img>
+      </div>
+      <p>The worlds most advanced yoga training platform. Built in React and also using Supabase and Mediapipe. <br></br>Contribution: Project manager and lead developer for application.</p>
+    </div>
+  );
+};
